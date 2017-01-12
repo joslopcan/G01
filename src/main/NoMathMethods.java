a072d3d3f83f9dab98b8a81adfd1a8a9d5dd3e21
joslopcan <joslopcan@alum.us.es>
2017-01-12 19:20:34 +0100

    Añadida nueva clase

---
+++ src/main/NoMathMethods.java
@@ -0,0 +1,15 @@
+package main;
+
+public class NoMathMethods {
+	
+	public static String cafe(Integer a){
+		String result;
+		if(a !=0){
+		result = "Hora del cafe";
+		}else{
+		result = "Tambien es hora del cafe";
+		}
+		return result;
+	}
+	
+}
