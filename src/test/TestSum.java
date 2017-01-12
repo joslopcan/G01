0d7a5660bb4f8b01eca5263ff3fc9ef2d387874a
joslopcan <joslopcan@alum.us.es>
2017-01-12 19:07:19 +0100

    Añadido mas tests

--- src/test/TestSum.java
+++ src/test/TestSum.java
@@ -12,9 +12,15 @@ public class TestSum extends TestCase {
 	}
 
 	public void testSumaDosNegativos() {
-		assertTrue(-3 == MathMethods.sum(-2, -2));
+		assertTrue(-4 == MathMethods.sum(-2, -2));
 	}
 	
-	
+	public void testProdDosPositivos() {
+		assertTrue(4 == MathMethods.sum(2, 2));
+	}
+
+	public void testProdDosNegativos() {
+		assertTrue(2 == MathMethods.sum(-1, -2));
+	}
 	
 }
