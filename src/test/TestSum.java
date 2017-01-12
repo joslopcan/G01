6b475f505d675e858656bd324de647d621a66b62
joslopcan <joslopcan@alum.us.es>
2017-01-12 18:58:55 +0100

    Commit en rama

---
+++ src/test/TestSum.java
@@ -0,0 +1,20 @@
+package test;
+
+import junit.framework.*;
+
+import main.MathMethods;
+
+public class TestSum extends TestCase {
+	
+	
+	public void testSumaDosPositivos() {
+		assertTrue(4 == MathMethods.sum(2, 2));
+	}
+
+	public void testSumaDosNegativos() {
+		assertTrue(-4 == MathMethods.sum(-2, -2));
+	}
+	
+	
+	
+}
